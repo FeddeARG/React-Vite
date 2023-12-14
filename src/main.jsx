d87da8dba8f6bf7d21fc6bee09a415146fdc1dc6
FeddeARG <federicoanaranjo@gmail.com>
2023-12-14 11:47:03 -0300

    VER1.2

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <ChakraProvider>
       <App />
       </ChakraProvider>
-  </React.StrictMode>,
 )
