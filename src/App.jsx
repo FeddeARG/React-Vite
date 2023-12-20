eb4b5b6b3738114339779dd5e669f7aba8666e1f
FeddeARG <federicoanaranjo@gmail.com>
2023-12-20 01:42:39 -0300

    falta2daEntrega

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar.jsx'
-import Cart from './components/Cart.jsx';
 import ItemListContainer from './components/ItemListContainer.jsx';
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CartWidget from './components/CartWidget.jsx';
 import ItemList from './components/ItemList.jsx';
 import Cart from './components/Cart.jsx';
+
 
 
 import ItemDetailContainer from './components/ItemDetailContainer.jsx'
@@ -20,11 +17,8 @@ const App = () => {    /* Pascalcase*/
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path='/' element={<main />} />
-        <Route path='/cart' element={<Cart />} />
         <Route path='/itemlist' element={<ItemList />} />
         <Route exact path='/' element={<ItemListContainer />} />
-        <Route exact path='/category/:categoryCalzados' element={<ItemListContainer />} />
         <Route exact path='/item/:id' element={<ItemDetailContainer />} />
         <Route exact path='/cart' element={<Cart />}></Route>
         <Route exact path='/detail' element={<ItemDetail />}></Route>
